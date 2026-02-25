62fe77fe3ce23af3e7727e248cde240daab53731
Ulka <cool.ulka84@gmail.com>
2026-02-25 21:37:09 +0200

    Add prompt/square

--- assets/js/index.js
+++ assets/js/index.js
@@ -8,13 +8,13 @@ console.log(c / d);
 
 let e = 4;
 let f = 7;
-sum = e+f;
+sum = e + f;
 console.log(sum);
 
 let number = 11;
 console.log(number);
 
-const itsTrue = number > 10;
+let itsTrue = number > 10;
 console.log(itsTrue);
 
 let text = "Java Script";
@@ -23,3 +23,15 @@ console.log(text);
 let stringNumber = "100";
 console.log(stringNumber);
 
+let num = 1;
+num += 11;
+num -= 11;
+num *= 11;
+num /= 11;
+num++;
+num--;
+
+const a1 = prompt("Введіть число");
+const result = Number(a1);
+let square = result ** 2;
+console.log("Result:", square);
