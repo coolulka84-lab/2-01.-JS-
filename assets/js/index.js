08b8f2bf2c6f8e4501701bccffbdc7c3be9272ad
Ulka <cool.ulka84@gmail.com>
2026-02-25 20:48:25 +0200

    Add Variable exercises

---
+++ assets/js/index.js
@@ -0,0 +1,25 @@
+let a = 5;
+let b = 10;
+console.log(a * b);
+
+let c = 15;
+let d = 3;
+console.log(c / d);
+
+let e = 4;
+let f = 7;
+sum = e+f;
+console.log(sum);
+
+let number = 11;
+console.log(number);
+
+const itsTrue = number > 10;
+console.log(itsTrue);
+
+let text = "Java Script";
+console.log(text);
+
+let stringNumber = "100";
+console.log(stringNumber);
+
