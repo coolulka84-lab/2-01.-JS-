c7103d1e79fb2a27aea0c4b3c62fd95e762d2d66
Ulka <cool.ulka84@gmail.com>
2026-02-25 22:27:23 +0200

    Add style.css

--- assets/js/index.js
+++ assets/js/index.js
@@ -35,3 +35,21 @@ const a1 = prompt("Введіть число");
 const result = Number(a1);
 let square = result ** 2;
 console.log("Result:", square);
+
+const b1 = prompt("Введіть перше число");
+const num1 = Number(b1);
+const b2 = prompt("Введіть друге число");
+const num2 = Number(b2);
+const resultB = (num1 + num2) / 2;
+console.log("Середнє арифметичне: ", resultB);
+
+let minute = prompt("Введіть кількість хвилин");
+let seconds = Number(minute) * 60;
+console.log("Кількість секунд: ", seconds);
+
+let greeting = "Hello, ";
+let userName;
+userName = prompt("What is your name?");
+let message = greeting + userName + "!";
+document.writeln("<p>" + message + "</p>");
+console.log(message);
